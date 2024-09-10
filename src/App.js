bb083cbb28f71697523e9bd2284bcb1c37b8849e
Wonderland6627 <994015020@qq.com>
2024-09-10 15:36:30 +0800

    添加备案号显示

--- src/App.js
+++ src/App.js
@@ -31,24 +31,35 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
   return (
-    <>
-      <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
-      </div>
-    </>
+    // <>
+    //   <div className="status">{status}</div>
+    //   <div className="board-row">
+    //     <Square value={squares[0]} onSquareClick={()=>handleClick(0)}/>
+    //     <Square value={squares[1]} onSquareClick={()=>handleClick(1)}/>
+    //     <Square value={squares[2]} onSquareClick={()=>handleClick(2)}/>
+    //   </div>
+    //   <div className="board-row">
+    //     <Square value={squares[3]} onSquareClick={()=>handleClick(3)}/>
+    //     <Square value={squares[4]} onSquareClick={()=>handleClick(4)}/>
+    //     <Square value={squares[5]} onSquareClick={()=>handleClick(5)}/>
+    //   </div>
+    //   <div className="board-row">
+    //     <Square value={squares[6]} onSquareClick={()=>handleClick(6)}/>
+    //     <Square value={squares[7]} onSquareClick={()=>handleClick(7)}/>
+    //     <Square value={squares[8]} onSquareClick={()=>handleClick(8)}/>
+    //   </div>
+    // </>
+    <div className="app">
+      <header>
+        {/* Header content */}
+      </header>
+      <main>
+        {/* Main content */}
+      </main>
+      <footer>
+        <a href="https://beian.miit.gov.cn/" target="_blank">ICP备案号：京ICP备2024066150号-1</a>
+      </footer>
+    </div>
   );
 }
 
